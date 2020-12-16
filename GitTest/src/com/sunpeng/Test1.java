38ac28211467d347161ce22f5366298bfed16af8
company <476567162@qq.com>
2020-12-16 17:29:11 +0800

    回退测试2

--- GitTest/src/com/sunpeng/Test1.java
+++ GitTest/src/com/sunpeng/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("sunpeng");
         System.out.println("hello 1");
+        System.out.println("hello 2");
     }
 }
