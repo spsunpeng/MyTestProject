d819604bb6ecc148f9d2a151751fa3ffcaa6945d
company <476567162@qq.com>
2021-01-11 13:28:47 +0800

    提交

--- GitTest/src/com/sunpeng/Test1.java
+++ GitTest/src/com/sunpeng/Test1.java
@@ -1,8 +1,10 @@
 package com.sunpeng;
 
+/**
+ * @author sunpeng
+ * @Date 2021-01-11 13:28
+ */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("test1");
-        System.out.println("test2");
     }
 }
